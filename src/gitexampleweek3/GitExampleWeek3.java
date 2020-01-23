c27a0c1e30d353539ccc6c38d3d447ba87493192
MananPathak13 <pathaman@sheridancollege.ca>
2020-01-23 09:30:02 -0500

    Second commit

--- src/gitexampleweek3/GitExampleWeek3.java
+++ src/gitexampleweek3/GitExampleWeek3.java
@@ -11,7 +11,7 @@ public class GitExampleWeek3 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("This is a modification of a file.");
     }
 
 }
